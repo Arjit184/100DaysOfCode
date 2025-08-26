5bdc0db5d09d1a83abdb25a30a8d812af9996b9c
Arjit184 <arjitpathak805@gmail.com>
2025-08-26 23:56:39 +0530

    Update si.c

--- day 5/si.c
+++ day 5/si.c
@@ -12,4 +12,12 @@ int main()
     printf("Simple Interest=%f\n",si);
     printf("Compound Interest=%f",ci);
     return 0;
+    /*
+        Enter principal,rate and time:1000 5 2
+Simple Interest=100.000000
+Compound Interest=102.500000
+
+        Enter principal,rate and time:5000 7 3
+Simple Interest=1050.000000
+Compound Interest=1125.214966
 }
