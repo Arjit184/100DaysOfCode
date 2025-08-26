bb4a556e1e64182c1b15dcd7facd39fc06559092
Arjit184 <arjitpathak805@gmail.com>
2025-08-26 23:37:24 +0530

    Update sum_n.c

--- day 4/sum_n.c
+++ day 4/sum_n.c
@@ -9,6 +9,14 @@ int main()
         sum=n*(n+1)/2;
         printf("Sum of first %d natural numbers is %d",n,sum);
         return 0;
+    /*
+        Enter a positive integer:5
+Sum of first 5 natural numbers is 15
+
+        Enter a positive integer:10
+Sum of first 10 natural numbers is 55
+        */
+        
     }
         
 
